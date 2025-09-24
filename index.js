13a57dcbcd3eb33441070f5f1f47ecdaa9ae0bc2
rajanR14042000 <rajanravi144@gmail.com>
2025-09-24 11:30:10 +0530

    a

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000" ;
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000" ||" https://r-rfrontend.vercel.app/" || "http://localhost:3001";
 
 // ---------- MIDDLEWARE ----------
 app.use(cors({
