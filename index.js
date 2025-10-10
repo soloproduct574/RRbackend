a8bdc7aa134affef00c17b81ce61869d360c562d
rajanR14042000 <rajanravi144@gmail.com>
2025-10-10 13:04:26 +0530

    main depeloy

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://r-rfrontend.vercel.app",
-  "https://keen-croquembouche-85becc.netlify.app"
+  "https://68e8b696cb15330835fd90c2--keen-croquembouche-85becc.netlify.app"
 ];
 
 // ---------- MIDDLEWARE ----------
